e3b7c94817fbade1f655c20c43f91475c7e21828
srajjn <145971425+srajjn@users.noreply.github.com>
2023-10-05 17:09:26 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123123
+let accountEmail = "srajan@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 121212     
+// const cant be change
+
+accountEmail = "sraj@google.com"
+accountPassword = "12121"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
