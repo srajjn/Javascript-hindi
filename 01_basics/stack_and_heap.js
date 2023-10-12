f6bc65fcd195d43b4492ae28e17085877f2b4600
srajjn <145971425+srajjn@users.noreply.github.com>
2023-10-12 10:26:55 +0000

    memory

---
+++ 01_basics/stack_and_heap.js
@@ -0,0 +1,17 @@
+let myyoutubename = "srajanjain.com"
+let anothername = myyoutubename
+anothername = "chaiorcode"
+
+// console.log(myyoutubename);
+// console.log(anothername);
+
+let userOne = {
+    email: "srajan@google.com",
+    upi: "jainsrajan@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "user@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
