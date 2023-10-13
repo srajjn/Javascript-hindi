85443e490945e5b102abbe9ce37313291889f9f4
srajjn <145971425+srajjn@users.noreply.github.com>
2023-10-13 18:13:45 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,25 @@
+const name = "Srajan"
+const repocount = 50
+// console.log(name + repocount + " value ")
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName = new String('SrajanSJ')
+// console.log(gameName[1]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-7,4)
+console.log(anotherString);
+
+const newStringone = "     Srajan   "
+console.log(newStringone);
+console.log(newStringone.trim());
+
